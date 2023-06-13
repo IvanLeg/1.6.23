78b368efc06f59fd143af96f126566edf616a4a1
IvanLeg <leguizamonivan97@gmail.com>
2023-06-12 22:00:46 -0300

    Initial commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    alert('leguizamonivan97@gmail.com');
+}
+function showAlert2() {
+    alert('1130814028');
+}
+function showAlert3() {
+    alert('Instagram: @peco.ws');
+}
+
